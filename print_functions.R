cf6e503ae7a9adad13085a9ac304dbd784e94131
Chethan4069 <chethans.23.beai@acharya.ac.in>
2024-09-17 18:40:58 +0530

    Create print_functions.R

---
+++ print_functions.R
@@ -0,0 +1,12 @@
+{
+  name=readline("Enter your name: ")
+paste("Hello All!!! my name is ",name)
+}
+{
+  name=readline("enter your name:")
+  sprintf("hello ALL!! my name is %s",name)
+}
+help(cat)
+x="hello"
+y="students"
+cat(x,y,file="demo.txt")
