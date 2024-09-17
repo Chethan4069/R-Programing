984a1f02ba35e0ad36eaab08e0a6a936b57773b5
Chethan4069 <chethans.23.beai@acharya.ac.in>
2024-09-17 16:20:31 +0530

    1stprogram.R

---
+++ 1stprogram.R
@@ -0,0 +1,20 @@
+#student details
+print("my name is chethan")
+print("usn=1AY23AI033")
+print("cgpa=9")
+help("reserved")
+a=2L
+class(a)
+typeof(a)
+c="hello"
+class(c)
+typeof(c)
+d='hello'
+class(d)
+typeof(d)
+e='H'
+f="R"
+class(e)
+class(f)
+typeof
+a=7
