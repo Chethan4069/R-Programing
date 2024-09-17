72ca8acb450f6939e77961ed671541774d253f98
Chethan4069 <chethans.23.beai@acharya.ac.in>
2024-09-17 18:42:22 +0530

    Create taking_input.R

---
+++ taking_input.R
@@ -0,0 +1,15 @@
+x=8
+y=10
+z=3
+rm(x)
+ls()
+rm(a,x,z)
+ls()
+._a=6
+ls()
+help("reserved")
+{
+var1=as.integer(readline("enter a value:"))
+var2=as.integer(readline("enter a value:"))
+}
+print(var1+var2)
